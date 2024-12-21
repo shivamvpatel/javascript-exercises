fa8b8347b6eff820b369f78b6d34ae2087e92c5a
Shivam Patel <127641586+shivamvpatel@users.noreply.github.com>
2024-12-21 10:21:21 -0800

    updated js for 02-repeatedString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    else {
+        let i = 0;
+        let printedString = "";
+        while (i < num) {
+            printedString += string;
+            i++;
+        }
+        return printedString;
+    }
 };
 
 // Do not edit below this line
