f5775317297dc5083a08f7e8357fcaf4c2d9412f
Shivam Patel <127641586+shivamvpatel@users.noreply.github.com>
2025-01-04 15:42:31 -0800

    updating exercises 05 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    sum = 0;
+    if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+        return "ERROR";
+    } else if (b > a) {
+        for (let i = a; i <= b; i++) {
+            sum += i;
+        }
+        return sum;
+    } else {
+        for (let i = b; i <= a; i++) {
+            sum += i;
+        }
+        return sum;
+    }
 };
-
 // Do not edit below this line
 module.exports = sumAll;
