1cbad38b51561dc57147f42f0e237d3210857e35
Shivam Patel <127641586+shivamvpatel@users.noreply.github.com>
2025-01-04 15:12:20 -0800

    update exercises 03 and 04 in JavaScript-exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let output = "";
+    let i = 1;
+    while (i <= word.length) {
+        output += (word[word.length-i]);
+        i++;
+    }
+    return output;
 };
+
+reverseString("hello world");
 
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...theArgs) {
+    return array.filter(item => !theArgs.includes(item));
 };
 
+// removeFromArray([1,2,3,4], 3);
 // Do not edit below this line
 module.exports = removeFromArray;
