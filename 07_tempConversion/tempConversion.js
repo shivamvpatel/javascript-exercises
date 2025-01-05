36dccb937941630912ba2ec320f07b3e7f997a2f
Shivam Patel <127641586+shivamvpatel@users.noreply.github.com>
2025-01-04 16:46:56 -0800

    update excercise 07 tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = ((fahrenheit - 32)*5)/9;
+  if (Number.isInteger(celsius)) {
+    return celsius;
+  } else {
+  return parseFloat(celsius.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = (celsius * 1.8) + 32;
+  if (Number.isInteger(fahrenheit)) {
+    return fahrenheit;
+  } else {
+  return parseFloat(fahrenheit.toFixed(1));
+  }
 };
 
 // Do not edit below this line
